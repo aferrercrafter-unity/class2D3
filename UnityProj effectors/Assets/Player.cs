b15dbaeb521e97df3bf8aa1e1f5218ba261c94c1
unknown <aferrercrafter@gmail.com>
2016-10-19 21:58:53 -0300

    first

---
+++ UnityProj effectors/Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+	void Awake ()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			rb.AddForce (Vector2.up * 10, ForceMode2D.Impulse);
+		}
+	}
+}
